77bd2f657c7d94bab0b510482ee15c4cd1ce3cd6
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-11-05 18:24:58 +0700

    api template

--- twitterapi/settings.py
+++ twitterapi/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = os.environ['secretkey']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+	'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
