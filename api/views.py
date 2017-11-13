af409bf3b126572fdc62106de0a3f5c085eddb09
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-11-13 18:42:52 +0700

    update save request
    update save request

--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_http_methods
 from TwitterSearch import *
 import pyrebase
 import os
 
-def search(request):
+def initializationDb():
 	config = {
 		"apiKey": os.environ['apiKey'],
 		"authDomain": os.environ['authDomain'],
@@ -21,34 +22,26 @@ def search(request):
 	user = auth.sign_in_with_email_and_password(email, password)
 
 	# Get a reference to the database service
-	db = firebase.database()
+	return firebase.database()
 	
+@require_http_methods(["GET"])
+def search(request):
+	db = initializationDb()
 	result = db.child("tweet").get(user['idToken'])
 	return JsonResponse(result.val())
 
+@require_http_methods(["POST"])
 def save(request):
-	config = {
-		"apiKey": os.environ['apiKey'],
-		"authDomain": os.environ['authDomain'],
-		"databaseURL": os.environ['databaseURL'],
-		"storageBucket": os.environ['storageBucket']
-	}
-	firebase = pyrebase.initialize_app(config)
-	email = os.environ['email']
-	password = os.environ['password']
-	# Get a reference to the auth service
-	auth = firebase.auth()
-
-	# Log the user in
-	user = auth.sign_in_with_email_and_password(email, password)
-
-	# Get a reference to the database service
-	db = firebase.database()
+	db = initializationDb()
 	
 	try:
+		lang = request.POST.get("lang", "en")
+		queryRaw = request.POST.get("query", "Film")
 		tso = TwitterSearchOrder()
-		tso.set_keywords(['Film', 'Batman'])
-		tso.set_language('en')
+		query = queryRaw.split()
+		tso.set_keywords(query)
+		if lang == 'en' or lang == 'id':
+			tso.set_language(lang)
 		
 		ts = TwitterSearch(
 			consumer_key = os.environ['consumerKey'],
@@ -60,6 +53,6 @@ def save(request):
 		for tweet in ts.search_tweets_iterable(tso):
 			# Pass the user's idToken to the push method
 			db.child("tweet").child(tweet['id']).set(tweet, user['idToken'])
-		return HttpResponse("Success save")
+		return JsonResponse({'status':200, 'message':'Success Saved to Database'})
 	except TwitterSearchException as e:
 		return HttpResponse(e)
