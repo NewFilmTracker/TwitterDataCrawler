38d7055a082a25e2dff1430cdfab376c34e0eb77
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-11-23 13:08:40 +0700

    change url

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	url(r'^release', views.retrieve_movie, name='release'),
 	url(r'^saverelease', views.movie_list, name='saverelease'),
 	url(r'^popular', views.retrieve_popular, name='popular'),
-	url(r'^search-movie', views.search_movie, name='search-movie'),
+	url(r'^savemovie', views.search_movie, name='savemovie'),
 	url(r'^savepopular', views.popular_movie, name='savepopular'),
 	url(r'^traindata', views.traindata, name='traindata')
 ]
